a5e7a5bc1525da470d79873dbde549e668ce3399
Henry Vo <133962400+hvo7@users.noreply.github.com>
2025-06-23 18:09:42 -0500

    Progress on RFM analysis

--- Analysis.sql
+++ Analysis.sql
@@ -1,21 +1,29 @@
--- SELECT
---   [CustomerID],
---   CONVERT(DATE, DATEADD(MONTH, DATEDIFF(MONTH, 0, [InvoiceDate]), 0)) AS ActivityMonth,
---   MIN(CONVERT(DATE, DATEADD(MONTH, DATEDIFF(MONTH, 0, [InvoiceDate]), 0)))
---       OVER (PARTITION BY [CustomerID]) AS CohortMonth
--- FROM [dbo].[Online_Retail_Cleaned]
--- WHERE [CustomerID] IS NOT NULL;
-
-
--- SELECT 
--- DISTINCT(InvoiceDate)
--- FROM dbo.Online_Retail_Cleaned
--- ORDER BY InvoiceDate
-
 -- SELECT *
 -- FROM dbo.Online_Retail_Cleaned
 -- WHERE TRY_CONVERT(DATETIME, [InvoiceDate]) IS NULL
 --   AND [InvoiceDate] IS NOT NULL
 --   AND LTRIM(RTRIM([InvoiceDate])) <> '';
--- we see a big problem in the way the data transferred into mssql. it all shifted to the right by one value. 
--- Ex: 536381	82567	"AIRLINE LOUNGE	METAL SIGN"	2	12/1/2010 9:41	2.1	15311,United Kingdom
+
+/* we see a big problem in the way the data transferred into mssql. it all shifted to the right by one value. 
+                InvoiceNo   StockCode            Description                    Quantity         InvoiceDate     UnitPrice CustomerID            Country
+     Bad Row Ex: 536381	        82567	    "AIRLINE LOUNGE	METAL SIGN"	            2	               12/1/2010        9:41	2.1	            15311,United Kingdom
+    Fixed the error by replacing commas in the Description column with ";" -> retransferred the db*/
+
+USE Online_Retail;
+
+GO
+
+-- Validate data by using Distinct on each column
+
+SELECT *
+FROM dbo.Online_Retail
+
+/* First, I will address:
+
+Which Customers do we target that are most likely to be retained?
+
+I will use Recency, Frequency, and Market Value analysis. */
+
+SELECT InvoiceDate, CustomerID
+FROM dbo.Online_Retail
+CAST(InvoiceDate AS Date) AS InvoiceDate
