09703fe8604345d452fe801285d9e07fe0ecf5f3
Henry Vo <133962400+hvo7@users.noreply.github.com>
2025-07-16 12:44:53 -0500

    Finished barebones of page 1 dashboard - mainly just added most relevant graphs. Need to still see if these answer all my relevant questions regarding OVERALL trends.

--- Dashboard/Data Visualization.sql
+++ Dashboard/Data Visualization.sql
@@ -45,32 +45,58 @@ GO
 -- ORDER BY PurchaseMonth;
 
 
-WITH First_Purchase AS (
-    SELECT
-        CustomerID,
-        Min(CAST(InvoiceDate AS DATE)) AS First_Purchase_Date
+-- WITH First_Purchase AS (
+--     SELECT
+--         CustomerID,
+--         Min(CAST(InvoiceDate AS DATE)) AS First_Purchase_Date
+--     FROM dbo.Online_Retail
+--     WHERE CustomerID IS NOT NULL
+--     GROUP BY CustomerID
+-- ),
+
+-- First_Purchase_Flag AS (
+--     SELECT
+--     a.CustomerID,
+--     CAST(a.InvoiceDate AS DATE) AS InvoiceDate,
+--     First_Purchase_Date,
+--     CASE WHEN   
+--         CAST(a.InvoiceDate AS DATE) = First_Purchase_Date THEN 1 
+--         ELSE 0
+--     END AS First_Purchase_Flag
+--     FROM dbo.Online_Retail a
+--     Join First_Purchase b ON a.CustomerID = b.CustomerID
+-- )
+
+-- SELECT 
+--     FORMAT(InvoiceDate, 'yyyy-MM') AS PurchaseMonth,
+--     COUNT(DISTINCT CustomerID) AS TotalCustomers,
+--     COUNT(DISTINCT CASE WHEN First_Purchase_Flag = 1 THEN CustomerID END) AS NewCustomers
+-- FROM First_Purchase_Flag
+-- GROUP BY FORMAT(InvoiceDate, 'yyyy-MM')
+-- ORDER BY PurchaseMonth
+
+
+WITH Max_Date AS (
+    SELECT 
+        MAX(CAST(InvoiceDate AS Date)) AS Analysis_Date
     FROM dbo.Online_Retail
-    WHERE CustomerID IS NOT NULL
+),
+
+Orders AS (
+    SELECT 
+        CustomerID,
+        MAX(CAST(InvoiceDate AS DATE))  AS recent_purchase,
+        COUNT(Distinct InvoiceNo) AS Num_orders,
+        ROUND(SUM(Revenue),2) AS Total_Revenue
+    FROM dbo.Online_Retail
+    WHERE CustomerID IS NOT NULL AND Revenue > 0
     GROUP BY CustomerID
 ),
 
-First_Purchase_Flag AS (
-    SELECT
-    a.CustomerID,
-    CAST(a.InvoiceDate AS DATE) AS InvoiceDate,
-    First_Purchase_Date,
-    CASE WHEN   
-        CAST(a.InvoiceDate AS DATE) = First_Purchase_Date THEN 1 
-        ELSE 0
-    END AS First_Purchase_Flag
-    FROM dbo.Online_Retail a
-    Join First_Purchase b ON a.CustomerID = b.CustomerID
+RFM AS (
+    SELECT 
+        CustomerID,
+        PERCENT_RANK() OVER (Order By Num_orders ASC) * 10 AS Frequency,
+        PERCENT_RANK() OVER (ORDER BY Total_Revenue ASC) * 10 AS Monetary
+    FROM Orders
 )
-
-SELECT 
-    FORMAT(InvoiceDate, 'yyyy-MM') AS PurchaseMonth,
-    COUNT(DISTINCT CustomerID) AS TotalCustomers,
-    COUNT(DISTINCT CASE WHEN First_Purchase_Flag = 1 THEN CustomerID END) AS NewCustomers
-FROM First_Purchase_Flag
-GROUP BY FORMAT(InvoiceDate, 'yyyy-MM')
-ORDER BY PurchaseMonth
