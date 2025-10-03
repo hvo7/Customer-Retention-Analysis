e85ab56795e7ac4107449f4e4db5159fb5f12990
Henry Vo <133962400+hvo7@users.noreply.github.com>
2025-10-03 12:29:07 -0500

    Finished Analyzing. Now I will create visualizations

--- Churn_Spike.sql
+++ Churn_Spike.sql
@@ -4,10 +4,9 @@ DECLARE @asof DATE = (
 	SELECT
 		MAX(CAST (InvoiceDate AS DATE)) 
 	FROM dbo.Online_Retail_Analysis
-)
+);
 
-
-WITH Orders AS 
+;WITH Orders AS 
 (
 	SELECT
 		CustomerID,
@@ -46,10 +45,44 @@ Repeat_Gaps AS
 	WHERE Prev_Purchase_Date IS NOT NULL		--Remove First Purchase dates
 ),
 
-T AS 
+Threshold AS 
 (
 -- Assuming that 80% of repeat purchases happen within T days. In this case T = 63
 	SELECT
 		 TOP 1 PERCENTILE_CONT(0.8) WITHIN GROUP (ORDER BY Days_Between ASC) OVER () AS T
 	FROM Repeat_Gaps
+),
+
+-- Now Get info per customer
+Customer AS 
+(
+	SELECT
+		CustomerID,
+		COUNT(DISTINCT(InvoiceDate)) AS Num_Orders,
+		MIN(InvoiceDate) AS First_Purchase,
+		MAX(InvoiceDate) AS Last_Purchase,
+		DATEDIFF(DAY, MIN(InvoiceDate), @asof) AS Days_From_Last
+	FROM dbo.Online_Retail_Analysis
+	GROUP BY CustomerID
 )
+
+SELECT
+	CustomerID,
+	Num_Orders,
+	First_Purchase,
+	Last_Purchase,
+	Days_From_Last,
+	Thres.T AS Churn_Critical_Window_Days,
+	CASE
+-- First time customer have initial grace period of T = 63 days, else they're at risk
+		WHEN Num_Orders <= 1 AND Days_From_Last < Thres.T THEN 'Grace Period'
+		WHEN Num_Orders <= 1 AND Days_From_Last >= Thres.T THEN 'At Risk - First_Customer'
+-- Repeat Customers are at risk if they are beyond grace period 
+		WHEN Num_Orders > 1 AND Days_From_Last > Thres.T THEN 'At Risk - Repeat Customer'
+		WHEN Num_Orders > 1 AND Days_From_Last >= 0.75*Thres.T THEN 'Approaching At-Risk - Repeat Customer'
+	ELSE 'Healthy'
+	END AS Churn_Risk_Group
+FROM Customer
+CROSS JOIN Threshold Thres
+WHERE CustomerID IS NOT NULL
+ORDER BY CustomerID ASC, Days_From_Last DESC
