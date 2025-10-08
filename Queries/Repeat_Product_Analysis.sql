a239959d644f5ec4c9f6472acd0d882926a47457
Henry <henryhuyvo74@gmail.com>
2025-10-08 01:10:41 -0500

    Updated

--- Queries/Repeat_Product_Analysis.sql
+++ Queries/Repeat_Product_Analysis.sql
@@ -1,5 +1,7 @@
 USE Online_Retail;
 
+-- Determining Repeat Rate
+/*
 WITH First_Orders AS 
 (
 	SELECT
@@ -58,7 +60,7 @@ SELECT
 		b.Description,
 		Repeat_Customers,
 		Unique_Customers,
-		ROUND((CAST(Repeat_Customers AS FLOAT)/CAST(Unique_Customers AS FLOAT)), 2) AS Repeat_Rate,
+		ROUND((CAST(Repeat_Customers AS FLOAT)/CAST(Unique_Customers AS FLOAT)), 4) AS Repeat_Rate,
 		CASE WHEN 
 			(ROUND((CAST(Repeat_Customers AS FLOAT)/CAST(Unique_Customers AS FLOAT)),2) > 0)
 			THEN 1 ELSE 0
@@ -77,5 +79,54 @@ LEFT JOIN Repeat_Rate b
 ON a.Description = b.Description
 WHERE a.Description IS NOT NULL AND CustomerID IS NOT NULL
 GROUP BY Repeat_Product_Flag
+*/
 
+WITH First_Orders AS 
+(
+	SELECT
+		CustomerID,
+		Description,
+		InvoiceDate,
+		Revenue,
+		MIN(InvoiceDate) OVER (PARTITION BY CustomerID, Description) AS First_Purchase
+	FROM dbo.Online_Retail_Analysis
+	WHERE CustomerID IS NOT NULL AND Description IS NOT NULL
+),
+
+Repeat_Flag AS 
+(
+	SELECT 
+		b.CustomerID,
+		b.Description,
+		b.InvoiceDate,
+		b.Revenue,
+		First_Purchase,
+		CASE WHEN 
+			b.InvoiceDate > First_Purchase 
+			THEN 1 ELSE 0 
+			END AS Repeat_Flag
+	FROM First_Orders a 
+	RIGHT JOIN dbo.Online_Retail_Analysis b
+	ON a.CustomerID = b.CustomerID 
+	AND a.Description = b.Description
+	AND a.InvoiceDate = b.InvoiceDate
+	WHERE b.Description IS NOT NULL AND b.CustomerID IS NOT NULL
+),
+
+Repeat_Customer_Flag AS 
+(
+	SELECT
+		CustomerID,
+		MAX(Repeat_Flag) AS Repeat_Customer_Flag
+	FROM Repeat_Flag
+	GROUP BY CustomerID
+)
+
+SELECT
+	Description,
+	SUM(Revenue)
+FROM Repeat_Flag a
+INNER JOIN Repeat_Customer_Flag b
+WHERE Repeat_Customer_Flag = 1
+GROUP BY Description
 
