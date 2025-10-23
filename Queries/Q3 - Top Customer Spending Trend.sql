14a3d3cbbcb5416b1b0f5029adba248a3bd3e301
Henry <henryhuyvo74@gmail.com>
2025-10-22 22:57:52 -0500

    Updated Q3

---
+++ Queries/Q3 - Top Customer Spending Trend.sql
@@ -0,0 +1,63 @@
+USE Online_Retail;
+
+WITH Monthly_Revenue AS 
+(
+	SELECT
+		a.CustomerID,
+		b.RFM_Score,
+		SUM(Revenue) AS Monthly_Revenue,
+		DATEFROMPARTS(YEAR(InvoiceDate), MONTH(InvoiceDate), 1) AS Month_Year,
+		CASE 
+			WHEN MONTH(InvoiceDate) <= 6 THEN 1
+			ELSE 2 
+		END AS Half
+	FROM dbo.Online_Retail_Analysis a
+	JOIN dbo.RFM_Analysis b
+		ON a.CustomerID = b.CustomerID
+	WHERE a.CustomerID IS NOT NULL
+		AND InvoiceNo NOT LIKE '%C%'
+	GROUP BY a.CustomerID,
+		b.RFM_Score,
+		YEAR(InvoiceDate),
+		MONTH(InvoiceDate)
+),
+
+Months AS 
+(
+	SELECT 
+		Distinct(Month_Year) AS Month_Year
+	FROM Monthly_Revenue
+)
+
+
+--Diffs AS (
+	SELECT
+		CustomerID,
+		RFM_Score,
+		b.Month_Year,
+		a.Half,
+		COALESCE(Monthly_Revenue,0),
+		COALESCE(Monthly_Revenue - LAG(Monthly_Revenue, 1) OVER (PARTITION BY CustomerID ORDER BY Month_Year ASC),0) AS Revenue_Diff
+	FROM Monthly_Revenue a
+	RIGHT JOIN Months b
+		ON a.Month_Year = b.Month_Year
+),
+
+Avg_Diffs AS 
+(
+	SELECT
+		CustomerID,
+		RFM_Score,
+		Half,
+		AVG(Revenue_Diff) AS Avg_Rev_Diff
+	FROM Diffs
+	GROUP BY CustomerID, RFM_Score, Half
+)
+
+SELECT
+	RFM_Score,
+	COUNT(RFM_Score) AS Number_Customers,
+	COALESCE(ROUND(Avg(Avg_Rev_Diff),2),0) AS Avg_Change_Rev
+FROM Avg_Diffs
+GROUP BY RFM_Score
+ORDER BY RFM_Score DESC
