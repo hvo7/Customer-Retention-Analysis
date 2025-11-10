72864699ab675479d7c56ff01dd3c113c7686592
Henry <henryhuyvo74@gmail.com>
2025-11-10 16:03:56 -0600

    Reframing the project

---
+++ Queries/Cohort_Analysis.sql
@@ -0,0 +1,30 @@
+USE Online_Retail;
+
+
+WITH Orders AS 
+(
+	SELECT
+		CustomerID,
+		DATEFROMPARTS(YEAR(InvoiceDate), MONTH(InvoiceDate), 1) AS Month_Year
+	FROM dbo.Online_Retail_Analysis
+	WHERE InvoiceNo NOT LIKE '%C%'
+		AND CustomerID IS NOT NULL
+	GROUP BY CustomerID, YEAR(InvoiceDate), MONTH(InvoiceDate)
+),
+
+First_Purchases AS 
+(
+	SELECT
+		CustomerID,
+		Month_Year,
+		MIN(Month_Year) OVER (PARTITION BY CustomerID) AS Cohort_Month
+	FROM Orders
+)
+
+SELECT
+	Cohort_Month,
+	Month_Year,
+	COUNT(DISTINCT(CustomerID)) AS Num_Cust
+FROM First_Purchases
+GROUP BY Cohort_Month, Month_Year
+ORDER BY Cohort_Month, Month_Year
--- Queries/Customer Retention Per Half.sql
+++ Queries/Customer Retention Per Half.sql
@@ -1,77 +1,116 @@
 USE Online_Retail;
 
+-- Divide our dataset into two halfs based on dates. Calculate Retention for both halves.
 
-DECLARE @CriticalWindow INT = 60;
-DECLARE @Midpoint DATE = '2011-06-05 22:38:00.0000000';
-
-WITH orders AS 
-(
-	SELECT
-	DISTINCT
-		CustomerID,
-		InvoiceDate,
-		ROUND(SUM(Revenue) OVER(PARTITION BY CustomerID),2) AS Total_Revenue
-	FROM dbo.Online_Retail_Analysis
-	WHERE InvoiceNo NOT LIKE '%C%'
-		AND CustomerID IS NOT NULL
-),
-
--- Calculate the midpoint datetime
--- Midpoint Datetime: 2011-06-05 22:38:00.0000000
-
---WIth Bounds AS (
---	SELECT 
---		MIN(InvoiceDate) AS MinD, 
---		MAX(InvoiceDate) AS MaxD
+DECLARE @Mid_Date datetime2 = '2011-06-05 08:26:00.0000000';
+-- Find midpoint of date
+-- Midpoint date = 2011-06-05 08:26:00.0000000
+--WITH Max_Min AS
+--(
+--	SELECT	
+--		Max(InvoiceDate) AS Max_Date,
+--		MIN(InvoiceDate) AS Min_Date
 --	FROM dbo.Online_Retail_Analysis
+--	WHERE InvoiceNo NOT LIKE '%C%'
 --)
 
 --SELECT
---	DATEADD(second, DATEDIFF(second, MinD, MaxD)/2, MinD) AS Midpoint
---FROM Bounds
+--	DATEADD(DAY, DATEDIFF(DAY, Min_Date, Max_Date) / 2, Min_Date) AS Mid_Date
+--FROM Max_Min
 
-Customer_Count AS
+
+WITH Orders AS 
+(
+	SELECT 
+		CustomerID,
+		TRY_CONVERT(datetime2, InvoiceDate) AS InvoiceDate
+	FROM dbo.Online_Retail_Analysis
+	WHERE InvoiceNo NOT LIKE '%C%'
+),
+
+First_Purchases AS 
+(
+	SELECT 
+		CustomerID,
+		MIN(InvoiceDate) AS First_Purchase
+	FROM dbo.Online_Retail_Analysis
+),
+
+Half AS
 (
 	SELECT
-		DISTINCT
-			CustomerID,
-	FROM orders
-	WHERE InvoiceDate 
-
-
-)
-
-Prev_Orders AS (
-	SELECT
 		CustomerID,
-		InvoiceDate,
-		lAG(InvoiceDate) OVER (PARTITION BY CustomerID ORDER BY InvoiceDate ASC) AS Prev_Order,
-		COALESCE(DATEDIFF(DAY, LAG(InvoiceDate) OVER (PARTITION BY CustomerID ORDER BY InvoiceDate ASC), InvoiceDate),0) AS Days_Since_Prev
-	FROM orders
+		DATEFROMPARTS(YEAR(InvoiceDate), MONTH(InvoiceDate), 1) AS Month_Year,
+		CASE WHEN InvoiceDate <= @Mid_Date
+		THEN 1
+		ELSE 2
+		END AS Half
+	FROM Orders
+),
+
+Bounds AS 
+(
+	SELECT
+		MIN(CASE WHEN Half = 1 THEN Month_Year END) AS H1_Min_Date,
+		MAX(CASE WHEN Half = 1 THEN Month_Year END) AS H1_Max_Date,
+		MIN(CASE WHEN Half = 2 THEN Month_Year END) AS H2_Min_Date,
+		MAX(CASE WHEN Half = 2 THEN Month_Year END) AS H2_Max_Date
+	FROM Half
+),
+
+Num_Cust AS
+(
+	SELECT
+		COUNT(DISTINCT
+				(CASE 
+				   WHEN Month_Year = H1_Min_Date AND Half = 1 THEN CustomerID ELSE NULL END
+				)
+			) AS H1_Start_Cust,
+
+		COUNT(DISTINCT
+				(CASE 
+				   WHEN Month_Year = H1_Max_Date AND Half = 1 THEN CustomerID ELSE NULL END
+				)
+			) AS H1_End_Cust,
+
+		COUNT(DISTINCT
+				(CASE 
+					WHEN Month_Year NOT IN (H1_Min_Date, H1_Max_Date) AND Half = 1 THEN CustomerID ELSE NULL END
+				)
+			) AS H1_New_Cust,
+
+		COUNT(DISTINCT
+				(CASE 
+				   WHEN Month_Year = H2_Min_Date AND Half = 2 THEN CustomerID ELSE NULL END
+				)
+			) AS H2_Start_Cust,
+
+		COUNT(DISTINCT
+			(CASE 
+				WHEN Month_Year = H2_Max_Date AND Half = 2 THEN CustomerID ELSE NULL END
+			)
+		) AS H2_End_Cust,
+
+		COUNT(DISTINCT
+				(CASE 
+					WHEN Month_Year NOT IN (H2_Min_Date, H2_Max_Date) AND Half = 2 THEN CustomerID ELSE NULL END
+				)
+			) AS H2_New_Cust
+	FROM 
+		Half a
+	CROSS JOIN Bounds b
+),
+
+Retention_Rates AS
+(
+	SELECT
+		H1_End_Cust - H1_New
+
+
 )
-
-SELECT
-	CustomerID,
-	InvoiceDate,
-	Prev_Order,
-	Days_Since_Prev,
-	CASE
-		WHEN Days_Since_Prev > 49 THEN 1
-		ELSE 0
-	END AS Churned_Flag
-FROM Prev_Orders
-WHERE Prev_Order IS NOT NULL
-ORDER BY CustomerID, InvoiceDate
-
--- Calculate the critical window
--- Critical Window = 49 days
-
 --SELECT
---	DISTINCT
---		CAST(PERCENTILE_CONT(0.8) WITHIN GROUP (ORDER BY Days_Since_Prev) OVER () AS INT) AS Critical_Window
---FROM Prev_Orders
-
--- Calcuated 49 Days as the crtiical window before a customer churns using the idea of the 80/20 rule (Crtiical window is within the top 80 percentage)
-
-
-
+--	a.Half,
+--	Start_Cust,
+--	End_Cust
+--FROM Start_Cust a
+--CROSS JOIN End_Cust b
