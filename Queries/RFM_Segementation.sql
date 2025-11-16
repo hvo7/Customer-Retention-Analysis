b157dd86ae23d5eddd1f9ff837b569eef2c1b568
Henry <henryhuyvo74@gmail.com>
2025-11-15 21:25:46 -0600

    Working on Calculating Freq Score

--- Queries/RFM_Segementation.sql
+++ Queries/RFM_Segementation.sql
@@ -16,7 +16,7 @@ RFM AS
 (
 	SELECT
 		CustomerID,
-		MAX(InvoiceDate) AS Recent_Purchase,
+		DATEDIFF(DAY, MAX(InvoiceDate), DATEFROMPARTS(2011, 12, 10)) AS Days_From_Last,
 		COUNT(DISTINCT(
 						DATEFROMPARTS(YEAR(InvoiceDate), Month(InvoiceDate), DAY(InvoiceDate))
 					   )
@@ -26,34 +26,121 @@ RFM AS
 	GROUP BY CustomerID
 ),
 
-Quartile_Vals AS
+Freq AS
 (
 	SELECT
-		MIN(PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY Recent_Purchase) OVER ()) AS R_q1,
-		MIN(PERCENTILE_CONT(0.50) WITHIN GROUP (ORDER BY Recent_Purchase) OVER ()) AS R_q2,
-		MIN(PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY Recent_Purchase) OVER ()) AS R_q3,
-
-		MIN(PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY Num_Orders) OVER ()) AS F_q1,
-		MIN(PERCENTILE_CONT(0.50) WITHIN GROUP (ORDER BY Num_Orders) OVER ()) AS F_q2,
-		MIN(PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY Num_Orders) OVER ()) AS F_q3,
-
-		MIN(PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY Total_Revenue) OVER ()) AS M_q1,
-		MIN(PERCENTILE_CONT(0.50) WITHIN GROUP (ORDER BY Total_Revenue) OVER ()) AS M_q2,
-		MIN(PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY Total_Revenue) OVER ()) AS M_q3
+		Num_Orders,
+		COUNT(*) AS Frequency
 	FROM RFM
+	GROUP BY Num_Orders
+),
+
+Freq_Score AS
+(
+	SELECT
+		Num_Orders,
+		Frequency,
+		LN(Frequency) AS Ln_Freq
+	FROM Freq
 )
 
---Scores AS 
---(
---SELECT 
---	CASE
---		WHEN Recent_Purchase < R_q1 THEN 4
---		WHEN Recent_Purchase < R_q2 THEN 3
---		WHEN Recent_Purchase < R_q3 THEN 2
---	ELSE 1
---	END AS R
+Quintile_Vals AS
+(
+	SELECT
+		PERCENTILE_CONT(0.20) WITHIN GROUP (ORDER BY Days_From_Last DESC) OVER () AS R_q1,
+		PERCENTILE_CONT(0.40) WITHIN GROUP (ORDER BY Days_From_Last DESC) OVER () AS R_q2,
+		PERCENTILE_CONT(0.60) WITHIN GROUP (ORDER BY Days_From_Last DESC) OVER () AS R_q3,
+		PERCENTILE_CONT(0.80) WITHIN GROUP (ORDER BY Days_From_Last DESC) OVER () AS R_q4,
 
-	
+		PERCENTILE_CONT(0.20) WITHIN GROUP (ORDER BY Num_Orders ASC) OVER () AS F_q1,
+		PERCENTILE_CONT(0.40) WITHIN GROUP (ORDER BY Num_Orders ASC) OVER () AS F_q2,
+		PERCENTILE_CONT(0.60) WITHIN GROUP (ORDER BY Num_Orders ASC) OVER () AS F_q3,
+		PERCENTILE_CONT(0.80) WITHIN GROUP (ORDER BY Num_Orders ASC) OVER () AS F_q4,
+
+		PERCENTILE_CONT(0.20) WITHIN GROUP (ORDER BY Total_Revenue ASC) OVER () AS M_q1,
+		PERCENTILE_CONT(0.40) WITHIN GROUP (ORDER BY Total_Revenue ASC) OVER () AS M_q2,
+		PERCENTILE_CONT(0.60) WITHIN GROUP (ORDER BY Total_Revenue ASC) OVER () AS M_q3,
+		PERCENTILE_CONT(0.80) WITHIN GROUP (ORDER BY Total_Revenue ASC) OVER () AS M_q4
+	FROM RFM
+),
+
+Calc_Scores AS 
+(
+SELECT 
+	CustomerID,
+	Days_From_Last,
+	Num_Orders,
+	Total_Revenue,
+
+	CASE
+		WHEN Days_From_Last > R_q1 THEN 1
+		WHEN Days_From_Last > R_q2 THEN 2
+		WHEN Days_From_Last > R_q3 THEN 3
+		WHEN Days_From_Last > R_q4 THEN 4
+	ELSE 5
+	END AS R,
+
+	CASE
+		WHEN Num_Orders = F_q1 THEN 1
+		WHEN Num_Orders = F_q2 THEN 2
+		WHEN Num_Orders >= F_q3 THEN 3
+		WHEN Num_Orders = F_q4 THEN 4
+	ELSE 5
+	END AS F,
+
+
+	CASE
+		WHEN Total_Revenue < M_q1 THEN 1
+		WHEN Total_Revenue < M_q2 THEN 2
+		WHEN Total_Revenue < M_q3 THEN 3
+		WHEN Total_Revenue < M_q4 THEN 4
+	ELSE 5
+	END AS M
+FROM RFM a
+CROSS JOIN Quintile_Vals b
+),
+
+Scores AS
+(
+SELECT
+	CustomerID,
+	Days_From_Last,
+	Num_Orders,
+	Total_Revenue,
+	R,
+	F,
+	M,
+	CONCAT(CAST(R AS varchar(1)), CAST(F AS varchar(1)), CAST(M AS varchar(1))) AS RFM,
+	R + F + M AS RFM_Sum
+From Calc_Scores
+GROUP BY CustomerID,
+	Days_From_Last,
+	Num_Orders,
+	Total_Revenue,
+	R,
+	F,
+	M,
+	CONCAT(CAST(R AS varchar(1)), CAST(F AS varchar(1)), CAST(M AS varchar(1)))
+),
+
+Segmentation AS
+(
+	SELECT
+	CustomerID,
+	R,
+	F,
+	M,
+	RFM,
+	RFM_Sum,
+	CASE 
+		WHEN RFM = '555' THEN 'Champions'
+		WHEN F = 1 AND M = 5 THEN 'Whales' -- High Spender, low frequency
+	END AS Segment
+
+	FROM Scores
+)
+
 SELECT
 	*
-FROM Quartile_Vals
+FROM Frequency
+ORDER BY Num_Orders ASC
