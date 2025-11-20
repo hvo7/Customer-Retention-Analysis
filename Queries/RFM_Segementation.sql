061d2aeb434046f392576dfb4785febc31aa1604
Henry <henryhuyvo74@gmail.com>
2025-11-20 15:28:35 -0600

    Updated Scoring of RFM

--- Queries/RFM_Segementation.sql
+++ Queries/RFM_Segementation.sql
@@ -33,20 +33,17 @@ RFM AS
 Quintile_Vals AS
 (
 	SELECT
-		PERCENTILE_CONT(0.20) WITHIN GROUP (ORDER BY Days_From_Last DESC) OVER () AS R_q1,
-		PERCENTILE_CONT(0.40) WITHIN GROUP (ORDER BY Days_From_Last DESC) OVER () AS R_q2,
-		PERCENTILE_CONT(0.60) WITHIN GROUP (ORDER BY Days_From_Last DESC) OVER () AS R_q3,
-		PERCENTILE_CONT(0.80) WITHIN GROUP (ORDER BY Days_From_Last DESC) OVER () AS R_q4,
+		PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY Days_From_Last DESC) OVER () AS R_q1,
+		PERCENTILE_CONT(0.50) WITHIN GROUP (ORDER BY Days_From_Last DESC) OVER () AS R_q2,
+		PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY Days_From_Last DESC) OVER () AS R_q3,
 
-		PERCENTILE_CONT(0.20) WITHIN GROUP (ORDER BY Ln_Num_Orders ASC) OVER () AS F_q1,
-		PERCENTILE_CONT(0.40) WITHIN GROUP (ORDER BY Ln_Num_Orders ASC) OVER () AS F_q2,
-		PERCENTILE_CONT(0.60) WITHIN GROUP (ORDER BY Ln_Num_Orders ASC) OVER () AS F_q3,
-		PERCENTILE_CONT(0.80) WITHIN GROUP (ORDER BY Ln_Num_Orders ASC) OVER () AS F_q4,
+		PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY Ln_Num_Orders ASC) OVER () AS F_q1,
+		PERCENTILE_CONT(0.50) WITHIN GROUP (ORDER BY Ln_Num_Orders ASC) OVER () AS F_q2,
+		PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY Ln_Num_Orders ASC) OVER () AS F_q3,
 
-		PERCENTILE_CONT(0.20) WITHIN GROUP (ORDER BY Total_Revenue ASC) OVER () AS M_q1,
-		PERCENTILE_CONT(0.40) WITHIN GROUP (ORDER BY Total_Revenue ASC) OVER () AS M_q2,
-		PERCENTILE_CONT(0.60) WITHIN GROUP (ORDER BY Total_Revenue ASC) OVER () AS M_q3,
-		PERCENTILE_CONT(0.80) WITHIN GROUP (ORDER BY Total_Revenue ASC) OVER () AS M_q4
+		PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY Total_Revenue ASC) OVER () AS M_q1,
+		PERCENTILE_CONT(0.50) WITHIN GROUP (ORDER BY Total_Revenue ASC) OVER () AS M_q2,
+		PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY Total_Revenue ASC) OVER () AS M_q3
 
 	FROM RFM
 ),
@@ -64,16 +61,14 @@ SELECT
 		WHEN Days_From_Last > R_q1 THEN 1
 		WHEN Days_From_Last > R_q2 THEN 2
 		WHEN Days_From_Last > R_q3 THEN 3
-		WHEN Days_From_Last > R_q4 THEN 4
-	ELSE 5
+	ELSE 4
 	END AS R,
 
 	CASE
 		WHEN Ln_Num_Orders <= F_q1 THEN 1
 		WHEN Ln_Num_Orders <= F_q2 THEN 2
 		WHEN Ln_Num_Orders <= F_q3 THEN 3
-		WHEN Ln_Num_Orders <= F_q4 THEN 4
-	ELSE 5
+	ELSE 4
 	END AS F,
 
 
@@ -81,8 +76,7 @@ SELECT
 		WHEN Total_Revenue < M_q1 THEN 1
 		WHEN Total_Revenue < M_q2 THEN 2
 		WHEN Total_Revenue < M_q3 THEN 3
-		WHEN Total_Revenue < M_q4 THEN 4
-	ELSE 5
+	ELSE 4
 	END AS M
 FROM RFM a
 CROSS JOIN Quintile_Vals b
@@ -99,8 +93,7 @@ SELECT
 	R,
 	F,
 	M,
-	CONCAT(CAST(R AS varchar(1)), CAST(F AS varchar(1)), CAST(M AS varchar(1))) AS RFM,
-	R + F + M AS RFM_Sum
+	CONCAT(CAST(R AS varchar(1)), CAST(F AS varchar(1)), CAST(M AS varchar(1))) AS RFM
 From Calc_Scores
 GROUP BY CustomerID,
 	Days_From_Last,
@@ -121,32 +114,36 @@ Segmentation AS
 	F,
 	M,
 	RFM,
-	RFM_Sum,
 	CASE 
-		WHEN RFM = '555' THEN 'Champions' 
-		WHEN R IN (4,5) AND F IN (3,4) AND M IN (3,4,5) THEN 'Potential Loyalists'
-		WHEN R IN (1,2) AND R + M >= 8 THEN 'High-Value At-Risk'
-		WHEN R = 3 AND F + M >= 8 THEN 'Loyal High-Value' 
-		WHEN R IN (2) AND F + M IN (7,8) THEN 'At-Risk'
-		WHEN R = 3 AND F IN (3,4) AND M IN (3,4) THEN 'Needing Attention'
-		WHEN R IN (4,5) AND F IN (1,2) THEN 'New Customers'
-		WHEN F = 1 AND M = 5 THEN 'Whales' -- High Spender, low frequency
-		WHEN R IN (1) AND F IN (1) THEN 'Lost'
+		WHEN RFM = '444' THEN 'Champions' -- 444
+		WHEN (R = 4 AND F = 4 ) 
+			OR (R = 3 AND F = 4 AND M IN (2,3,4) )								
+			OR	(R = 4 AND F = 3 AND M IN (2,3,4) )	THEN 'Loyalist'		
+		WHEN (R = 3 AND F = 2  THEN 'Potential Loyalist'
+		WHEN RFM IN ('424', '414', '413', '423') THEN 'Promising'
+		WHEN R IN (3,4) AND F = 1 THEN 'New Customers'
+		WHEN RFM IN ('322', '323', '332', '333') THEN 'Need Attention' 
+		WHEN RFM IN ('244', '243', '234', '224', '214', '144', '143', '134', '233') THEN 'At-Risk High Value'
+		WHEN RFM IN ('211', '212', '221', '222', '111','112', '113', '114', '121', '122') THEN 'Lost' 
+	ELSE 'Other'
 	END AS Segment
 	FROM Scores
 )
 
-SELECT
-	CustomerID,
-	RFM,
-	Segment
-FROM Segmentation
-WHERE Segment IS NULL 
-ORDER BY CustomerID
 
---SELECT
---	COUNT(*) AS Count
---FROM Segmentation
---WHERE Segment IS NULL
+
+-- 4 = Highest
+-- 3 = High
+-- 2 = Med
+-- 1  = Low
+
+
+SELECT
+	DISTINCT(RFM)
+FROM Segmentation
+WHERE Segment = 'Other'
+
 
 --4339 Total Customers
+
+
