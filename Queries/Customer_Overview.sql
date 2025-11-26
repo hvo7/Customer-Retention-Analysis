43a9ddf48d9e8fad6056d3416570c6d99b083a39
Henry <henryhuyvo74@gmail.com>
2025-11-26 00:28:35 -0600

    Finshed Analysis, now working on presenting deliverables in slide deck

--- Queries/Customer_Overview.sql
+++ Queries/Customer_Overview.sql
@@ -1,7 +1,20 @@
 USE Online_Retail;
 
+WITH Purchases AS
+(
+	SELECT
+		InvoiceNo,
+		CAST(InvoiceDate AS DATE) AS InvoiceDate,
+		SUM(Revenue) AS Total_Revenue
+	FROM dbo.Online_Retail_Analysis
+	WHERE InvoiceNo NOT LIKE '%C%'
+	GROUP BY InvoiceNo, CAST(InvoiceDate AS DATE)
+
+)
+
 SELECT
-	COUNT(DISTINCT CustomerID) AS Total_Customers
-FROM dbo.Online_Retail_Analysis
-WHERE CustomerID IS NOT NULL
-	AND InvoiceNo NOT LIKE '%C%'
+	--COUNT(DISTINCT CustomerID) AS Total_Customers
+	--ROUND(SUM(Revenue),2) AS Total_Revenue
+	SUM(Total_Revenue) AS Overall_Revenue
+	--COUNT(InvoiceNo) AS Total_Orders
+FROM Purchases
